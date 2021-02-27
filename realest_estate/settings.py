1d6d7f045a92957a7884de67bade4d9b4f1cc94c
hro-maker <69071631+hro-maker@users.noreply.github.com>
2021-02-27 12:09:25 +0400

    m

--- realest_estate/settings.py
+++ realest_estate/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'realest_estate',
         'USER': 'postgres',
-        'PASSWORD': '[YOUR POSTGRES DATABASE PASSWORD]',
+        'PASSWORD': '123456',
         'HOST': 'localhost'
     }
 }
@@ -92,7 +92,7 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = '[YOUR EMAIL]'
+EMAIL_HOST_USER = 'hrantmuradyan137@gmail.com'
 EMAIL_HOST_PASSWORD = '[YOUR APP PASSWORD]'
 EMAIL_USE_TLS = True
 
